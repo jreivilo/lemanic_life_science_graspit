096f947a77ae218575ff971b19228a979fe16952
jreivilo <j.reivilo@outlook.com>
2025-04-23 16:01:58 +0200

    update link in dev script

--- dev/publisher.py
+++ dev/publisher.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import time
 
 client = mqtt.Client()
-client.connect("localhost", 1883, 60)
+client.connect("cluster.jolivier.ch", 1883, 60)
 
 # Give some time to establish connection
 while True:
--- dev/subscriber.py
+++ dev/subscriber.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 # Callback when connected to broker
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe("test/topic")
+    client.subscribe("/infrared_camera")
 
 # Callback when a message is received
 def on_message(client, userdata, msg):
@@ -13,6 +13,6 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect("cluster.jolivier.ch", 1883, 60)
 client.loop_forever()
 
